0266fe00cd595ba3a944bdfe5e2b4f5878a62e2a
mval23 <111825282+mval23@users.noreply.github.com>
2023-10-21 13:46:16 -0500

    Decimals update

--- src/gestorAplicacion/campeonato/Decimales.java
+++ src/gestorAplicacion/campeonato/Decimales.java
@@ -4,24 +4,12 @@ import java.text.DecimalFormat;
 public interface Decimales {
 
     default double dosDecimales(double valor) {
-        DecimalFormat formatoDecimal = new DecimalFormat("#.00");
-        String valorFormateado = formatoDecimal.format(valor);
-
-        // Parseamos el valor formateado de vuelta a un double
-        double valorRedondeado = Double.parseDouble(valorFormateado);
-
-        return valorRedondeado;
+        return Math.round(valor * 100.0) / 100.0;
     }
     void redondear();
 
 
     static double dosDecimalesP(double valor) {
-        DecimalFormat formatoDecimal = new DecimalFormat("#.00");
-        String valorFormateado = formatoDecimal.format(valor);
-
-        // Parseamos el valor formateado de vuelta a un double
-        double valorRedondeado = Double.parseDouble(valorFormateado);
-
-        return valorRedondeado;
+        return Math.round(valor * 100.0) / 100.0;
     }
 }
